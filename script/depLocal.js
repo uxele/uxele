71a878b46249404ae1ac85be68c50eeb5b62ed54
KeyangXiang <keyang.xiang@gmail.com>
2018-11-05 23:33:08 +0000

    Push all script

---
+++ script/depLocal.js
@@ -0,0 +1,14 @@
+var path=require("path");
+var pkg=require("../package.json");
+var deps=pkg.dependencies;
+var psdetchPkgs=[];
+var exec=require("child_process").execSync;
+for (var key in deps){
+  if (key.indexOf("psdetch-")===0){
+    psdetchPkgs.push(key);
+  }
+}
+console.log(pkg.name,"links to:", psdetchPkgs);
+psdetchPkgs.forEach(function(pkg){
+  exec("npm link "+pkg);
+});
